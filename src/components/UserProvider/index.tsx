0e39c0fd16f19273040876d4dae958ca1883c591
Kamal Mukkamala <kamalnrf@gmail.com>
2022-10-06 19:56:44 +0530

    Add changes to UserProvider to set user details

--- src/components/UserProvider/index.tsx
+++ src/components/UserProvider/index.tsx
@@ -1,29 +1,35 @@
 import {createContext, useState, useContext} from 'react'
 
-type State = {
+type UserDetails = {
   name: string
   token: string
   username: string
 }
 
-type ContexValues = {
+type ContextValues = {
   isUserSignedIn: boolean
-  setUser: React.Dispatch<React.SetStateAction<State>>
-} & State
+  setUserDetails: (userDetails: UserDetails) => void
+} & UserDetails
 
 type Props = {
   children: React.ReactNode
 }
 
-const UserConext = createContext<null | ContexValues>(null)
+const UserConext = createContext<null | ContextValues>(null)
 
 function UserProvider({children}: Props) {
   const userInLocalStorage = JSON.parse(localStorage.getItem('user') ?? '{}')
-  const [user, setUser] = useState<State>(userInLocalStorage)
+  const [user, setUser] = useState<UserDetails>(userInLocalStorage)
+
+  const setUserDetails = (userDetails: UserDetails) => {
+    localStorage.setItem('user', JSON.stringify(userDetails))
+    setUser(userDetails)
+  }
+
   const value = {
     ...user,
     isUserSignedIn: Boolean(user.token),
-    setUser,
+    setUserDetails,
   }
 
   return <UserConext.Provider value={value}>{children}</UserConext.Provider>
