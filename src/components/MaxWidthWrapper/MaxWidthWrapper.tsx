2581c19922300a7c6148765133381fd03f212f70
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-10-07 17:21:36 +0530

    Add MaxWidthWrapper and Layout changes

---
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+// const MaxWidthWrapper = (props: any) => {
+//   return <Container {...props} />
+// }
+
+const MaxWidthWrapper = styled.div`
+  width: 650px;
+  height: 100%;
+  margin: 0 auto;
+`
+
+export {MaxWidthWrapper}
