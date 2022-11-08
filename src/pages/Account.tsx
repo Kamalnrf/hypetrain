c6e0815ec47d84db9681c1344c961514bf1d8fc7
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-11-08 17:56:39 +0530

    Add changes to fix layout for Account, Activity and Home

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -8,43 +8,42 @@ export default function Home() {
   const {logout} = useUser()
 
   return (
-    <>
-      <Actions>
-        <ItemWrapper onClick={logout}>
-          <IconContainer>
-            <LogoutIcon width={108} height={84} />
-          </IconContainer>
-          logout
-        </ItemWrapper>
-        <ItemWrapper>
-          <IconContainer>
-            <StyledDeleteIcon width={128} height={128} />
-          </IconContainer>
-          delete
-        </ItemWrapper>
-      </Actions>
-    </>
+    <Actions>
+      <ItemWrapper onClick={logout}>
+        <IconContainer>
+          <LogoutIcon width={72} />
+        </IconContainer>
+        logout
+      </ItemWrapper>
+      <ItemWrapper>
+        <IconContainer>
+          <StyledDeleteIcon width={84} />
+        </IconContainer>
+        delete
+      </ItemWrapper>
+    </Actions>
   )
 }
+
+const Actions = styled.div`
+  color: var(--white);
+  font-size: 2rem;
+
+  @media ${QUERIES.mobile} {
+    font-size: 1.5rem;
+    margin-inline-start: 16px;
+  }
+`
 
 const ItemWrapper = styled.div`
   display: flex;
   align-items: center;
-  margin-block-end: 32px;
-`
-
-const Actions = styled.div`
-  color: var(--white);
-  font-size: 3rem;
-
-  @media ${QUERIES.mobile} {
-    font-size: 1.5rem;
-  }
+  margin-block-end: 12px;
 `
 
 const IconContainer = styled.div`
-  max-width: 144px;
-  height: 72px;
+  width: 92px;
+  height: 80px;
   margin-inline-end: 24px;
 
   @media ${QUERIES.mobile} {
