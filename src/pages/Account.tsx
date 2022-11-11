fd40bca1eab16822d84e7e6cc07f3e52862f54f7
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-11-11 10:34:38 +0530

    Add Logout Modal screen

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -3,12 +3,21 @@ import {useUser} from '../components/UserProvider'
 import styled from 'styled-components'
 import {ReactComponent as LogoutIcon} from '../assets/logout.svg'
 import {ReactComponent as DeleteIcon} from '../assets/delete.svg'
+import {ReactComponent as Highlight} from '../assets/highlight.svg'
+import {ReactComponent as HappySmiley} from '../assets/smiley-happy.svg'
 import {QUERIES} from '../constants'
+import {Modal} from '../components/Modal'
+import {SelfDestruct} from '../components/SelfDestruct'
 
 export default function Home() {
   const mobileMatch = window.matchMedia(QUERIES.mobile)
   const {logout} = useUser()
   const [isMobile, setMobile] = useState(mobileMatch.matches)
+  const [isVisible, setVisible] = useState(false)
+
+  function handleClick() {
+    setVisible(!isVisible)
+  }
 
   useEffect(() => {
     function onChange(event: MediaQueryListEvent) {
@@ -23,7 +32,7 @@ export default function Home() {
 
   return (
     <Actions>
-      <ItemWrapper onClick={logout}>
+      <ItemWrapper onClick={handleClick}>
         <IconContainer>
           <LogoutIcon width={isMobile ? 52 : 72} height={'auto'} />
         </IconContainer>
@@ -35,6 +44,16 @@ export default function Home() {
         </IconContainer>
         delete
       </ItemWrapper>
+      <Modal backgroundColor="var(--white)" isVisible={isVisible}>
+        <SelfDestruct destroyAfter={3000} onComplete={logout}>
+          <Wrapper>
+            <StyledHighlight />
+            <LogoutIcon />
+            <Text>see you later!</Text>
+            <StyledHappySmiley />
+          </Wrapper>
+        </SelfDestruct>
+      </Modal>
     </Actions>
   )
 }
@@ -53,6 +72,8 @@ const ItemWrapper = styled.div`
   display: flex;
   align-items: center;
   margin-block-end: 12px;
+  cursor: pointer;
+  width: fit-content;
 
   @media ${QUERIES.mobile} {
     margin-block-end: 24px;
@@ -67,5 +88,48 @@ const IconContainer = styled.div`
     display: flex;
     align-items: center;
     width: 64px;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 420px;
+  font-size: 2rem;
+  margin-block-start: -32px;
+  position: relative;
+
+  @media ${QUERIES.mobile} {
+    width: 280px;
+    font-size: 1.5rem;
+  }
+`
+
+const StyledHighlight = styled(Highlight)`
+  width: 56px;
+  position: absolute;
+  top: -36px;
+  left: -28px;
+
+  @media ${QUERIES.mobile} {
+    width: 40px;
+    top: -34px;
+    left: -22px;
+  }
+`
+
+const Text = styled.p`
+  color: var(--black);
+  margin-block-start: 24px;
+`
+
+const StyledHappySmiley = styled(HappySmiley)`
+  width: 100px;
+  position: absolute;
+  bottom: -64px;
+  right: 32px;
+
+  @media ${QUERIES.mobile} {
+    width: 75px;
+    bottom: -56px;
+    right: 0px;
   }
 `
