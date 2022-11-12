87ba81e08bc67776544b6726284679f64730b751
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-11-13 05:12:16 +0530

    Add Landing Page for initial release

--- src/components/SignInButton/SignInButton.tsx
+++ src/components/SignInButton/SignInButton.tsx
@@ -1,3 +1,6 @@
+import styled from 'styled-components'
+import {QUERIES} from '../../constants'
+
 const CLIENT_ID = process.env.REACT_APP_TWITTER_CLIENT_ID
 const REDIRECT_URI = process.env.REACT_APP_TWITTER_REDIRECT_URI
 const OAUTH_URL = 'https://twitter.com/i/oauth2/authorize'
@@ -19,9 +22,33 @@ const CODE_CHALLENGE_STATE = 'state'
 const TWITTER_AUTH_URL = `${OAUTH_URL}?response_type=${OAUTH_RESPONSE_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${OAUTH_SCOPES}&state=${CODE_CHALLENGE_STATE}&code_challenge=${CODE_CHALLENGE}&code_challenge_method=${CODE_CHALLENGE_METHOD}`
 
 const SignInButton = () => (
-  <a href={TWITTER_AUTH_URL} tabIndex={-1}>
-    <button>Sign in with Twitter!</button>
-  </a>
+  <Wrapper>
+    <AuthLink href={TWITTER_AUTH_URL} tabIndex={-1}>
+      <JoinButton>Join Hypetrain!</JoinButton>
+    </AuthLink>
+  </Wrapper>
 )
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const AuthLink = styled.a``
+
+const JoinButton = styled.button`
+  font-size: 2rem;
+  padding-block: 16px;
+  padding-inline: 48px;
+  border: none;
+  outline: none;
+  border-radius: 10000px;
+  background-color: var(--green);
+  cursor: pointer;
+
+  @media ${QUERIES.mobile} {
+    font-size: 1.5rem;
+  }
+`
 
 export {SignInButton}
